79a6a72c25e33d6811d79e91cdf4f70f0aec21a1
barte28 <bartek.teuerle@gmail.com>
2024-10-31 08:55:32 +0100

    koniec lekcji

--- app/src/main/java/com/example/gra/MainActivity.java
+++ app/src/main/java/com/example/gra/MainActivity.java
@@ -3,13 +3,19 @@ package com.example.gra;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<ImageView> listaWidokowJablka = new ArrayList<>();
+    CountDownTimer countDownTimer;
+    TextView textView;
+    int czas = 1000*10;
+    int licznik = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,11 +31,40 @@ public class MainActivity extends AppCompatActivity {
         listaWidokowJablka.add(findViewById(R.id.imageView9));
         ukryjJablka();
         pokazLosoweJablkow();
+        odliczanieCzasu();
+    }
+
+    private void odliczanieCzasu(){
+        countDownTimer = countDownTimer = new CountDownTimer(czas, 1000) {
+            @Override
+            public void onTick(long l) {
+                ukryjJablka();
+                pokazLosoweJablkow();
+                czas = (int) l;
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        };
+        countDownTimer.start();
     }
 
     private void pokazLosoweJablkow(){
         int losowa = (int) (Math.random()*9);
         listaWidokowJablka.get(losowa).setVisibility(View.VISIBLE);
+        textView = findViewById(R.id.TextViewPunkty);
+        listaWidokowJablka.get(losowa).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                licznik++;
+                view.setVisibility(View.INVISIBLE);
+                pokazLosoweJablkow();
+                textView.setText(licznik+" Trafionych");
+
+            }
+        });
     }
 
     private void ukryjJablka(){
